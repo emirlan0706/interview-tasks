d0a8b5ffc66b4fd1a080e0c07ae2d7af024f29e9
Emirlan <abdykadyrov.emirlan.00@gmail.com>
2023-09-27 16:24:35 +0600

    depp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  // code
+  // code;
   const decode = (encoded) => {
     const splitValues = encoded.split(".").map(Number);
     const groupedValues = [];
